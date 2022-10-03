a7aeb2f41544b202548d8a17890b1da72247589e
Rahul Menon <rahul.menon85280@gmail.com>
2022-10-03 19:02:57 -0400

    test

--- runcar.java
+++ runcar.java
@@ -5,7 +5,7 @@ public class runcar {
     public static void main(String[] args){
         Car myOldCar = new Car(2000, "Toyota", "Camry");
         Car myNewCar = new Car(2022, "Toyota", "GR86");
-
+        
         System.out.println(myOldCar);
         System.out.println(myNewCar);
     }
