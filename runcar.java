fbfb68190d3579bf483aedf85c67c0dcf531d7ce
Rahul Menon <rahul.menon85280@gmail.com>
2022-10-03 15:28:04 -0400

    Created a new car thing

---
+++ runcar.java
@@ -0,0 +1,14 @@
+public class runcar {
+    /**
+     * @param args
+     */
+    public static void main(String[] args){
+        Car myOldCar = new Car(2000, "Toyota", "Camry");
+        Car myNewCar = new Car(2022, "Toyota", "GR86");
+
+        System.out.println(myOldCar);
+        System.out.println(myNewCar);
+    }
+
+
+}
