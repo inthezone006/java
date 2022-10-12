912a2e504faff1cfaf5447bc383ab45cfdc3950e
Rahul Menon <rahul.menon85280@gmail.com>
2022-10-12 17:52:43 -0400

    got some sort of code that works

--- LegoSetCompetition.java
+++ LegoSetCompetition.java
@@ -9,116 +9,148 @@ public class LegoSetCompetition{
         ArrayList<String> legosets = new ArrayList<String>();
         ArrayList<Integer> pieces = new ArrayList<Integer>();
 
-        int i = 1;
         String currentName = "";
         int currentPieces = 0;
-        while(true){
-            System.out.println("Enter the name of Lego Set " + i);
-            currentName = scan.nextLine();
-            System.out.println("Enter the number of pieces of Lego Set " + i);
-            currentPieces = scan.nextInt();
-            scan.nextLine();
-            legosets.add(currentName);
-            pieces.add(currentPieces);
-            i++;
-            if (i == 4){
-                break;
-            }
-        }
-
-        int total = pieces.get(0) + pieces.get(1) + pieces.get(2);
-
-        int days = 1;
-        int player1 = 0;
-        int player2 = 0;
 
         boolean gameOn = true;
-        int winner = 0;
-
-        ArrayList<String> player1complete = new ArrayList<String>();
-        ArrayList<String> player2complete = new ArrayList<String>();
-        ArrayList<String> player1incomplete = new ArrayList<String>(legosets);
-        ArrayList<String> player2incomplete = new ArrayList<String>(legosets);
-
-
-        while(gameOn){
-            System.out.println("Enter the number of pieces player 1 used for building on day " + days);
-            player1 = player1 + scan.nextInt();
-            System.out.println("Enter the number of pieces player 2 used for building on day " + days);
-            player2 = player2 + scan.nextInt();
-            days++;
-
-
-            if (player1 >= total || player2 >= total){
-                gameOn = false;
-                
-                if (player1 > pieces.get(0)){
-                    player1complete.add(legosets.get(0));
-                    if (player1 > pieces.get(0) + pieces.get(1)){
-                        player1complete.add(legosets.get(1));
-                        if (player1 > pieces.get(0) + pieces.get(1) + pieces.get(2)){
-                            player1complete.add(legosets.get(2));
-                        }
-                    } 
-                }
-
-                if (player2 > pieces.get(0)){
-                    player2complete.add(legosets.get(0));
-                    if (player2 > pieces.get(0) + pieces.get(1)){
-                        player2complete.add(legosets.get(1));
-                        if (player2 > pieces.get(0) + pieces.get(1) + pieces.get(2)){
-                            player2complete.add(legosets.get(2));
-                        }
-                    } 
-                }
-                
-                if (player1 >= total){
-                    //if player 1 wins
-                    winner = 1;
-                } 
-
-                else if (player2 >= total){
-                    //if player 2 wins
-                    winner = 2;
-                }
-
-                if (player1 >= total && player2 >= total){
-                      //only keep days and completed sets
-                        
-                }
-                
-
-            }
-        }
+        
+        int player1 = 0;
+        int player2 = 0;
 
         String player1completed = "";
         String player2completed = "";
         String player1incompleted = "";
         String player2incompleted = "";
 
-        //formatting
+        int winner = 0;
 
-        for (int x = 0; x < player1complete.size(); x++){
-            player1completed = player1completed + player1complete.get(x) + ", ";
+        int days = 1;
+
+        while(gameOn){
+            for (int u = 1; u < 4; u++){
+                System.out.println("Enter the name of Lego Set " + u);
+                currentName = scan.nextLine();
+                System.out.println("Enter the number of pieces of Lego Set " + u);
+                currentPieces = scan.nextInt();
+                scan.nextLine();
+                legosets.add(currentName);
+                pieces.add(currentPieces);
+            }
+        
+
+            int total = pieces.get(0) + pieces.get(1) + pieces.get(2);
+
+            ArrayList<String> player1complete = new ArrayList<String>();
+            ArrayList<String> player2complete = new ArrayList<String>();
+            ArrayList<String> player1incomplete = new ArrayList<String>(legosets);
+            ArrayList<String> player2incomplete = new ArrayList<String>(legosets);
+
+            int player1game = 0;
+            int player2game = 0;
+
+            boolean gamePlay = true;
+
+            while(gamePlay){
+                System.out.println("Enter the number of pieces player 1 used for building on day " + days);
+                player1game = player1game + scan.nextInt();
+                player1 = player1 + player1game;
+                System.out.println("Enter the number of pieces player 2 used for building on day " + days);
+                player2game = player2game + scan.nextInt();
+                player2 = player2 + player2game;
+                days++;
+
+
+                if (player1game >= total || player2game >= total){
+                    gameOn = false;
+                
+                    if (player1game >= pieces.get(0)){
+                        player1complete.add(legosets.get(0));
+                        player1incomplete.remove(0);
+                        if (player1game >= pieces.get(0) + pieces.get(1)){
+                            player1complete.add(legosets.get(1));
+                            player1incomplete.remove(1);
+                            if (player1game >= pieces.get(0) + pieces.get(1) + pieces.get(2)){
+                                player1complete.add(legosets.get(2));
+                                player1incomplete.removeAll(player2incomplete);
+                            }
+                        } 
+                    }
+
+                    if (player2game >= pieces.get(0)){
+                        player2complete.add(legosets.get(0));
+                        player2incomplete.remove(0);
+                        if (player2game >= pieces.get(0) + pieces.get(1)){
+                            player2complete.add(legosets.get(1));
+                            player2incomplete.remove(1);
+                            if (player2game >= pieces.get(0) + pieces.get(1) + pieces.get(2)){
+                                player2complete.add(legosets.get(2));
+                                player2incomplete.removeAll(player2incomplete);
+                            }
+                        } 
+                    }
+                
+                    if (player1game >= total){
+                        //if player 1 wins
+                        winner = 1;
+                        gameOn = false;
+                        gamePlay = false;
+                    } 
+
+                    else if (player2game >= total){
+                        //if player 2 wins
+                        winner = 2;
+                        gameOn = false;
+                        gamePlay = false;
+                    }
+
+                    if (player1game >= total && player2game >= total){
+                        //only keep days and completed sets
+                        gameOn = true;
+                        gamePlay = false;
+                        System.out.println("The competition ended in a tie! There will be a tiebreaker round");
+                        player1game = 0;
+                        player2game = 0;
+                        scan.nextLine();
+                        legosets.removeAll(legosets);
+                        pieces.removeAll(pieces);
+                        player1complete.removeAll(player1complete);
+                        player2complete.removeAll(player2complete);  
+                    }
+                }
+            }
+
+            
+
+            //formatting
+
+            for (int x = 0; x < player1complete.size(); x++){
+                player1completed = player1completed + player1complete.get(x) + ", ";
+            }
+
+            for (int y = 0; y < player2complete.size(); y++){
+                player2completed = player2completed + player2complete.get(y) + ", ";
+            }
+
+            for (int t = 0; t < player1incomplete.size(); t++){
+                player1incompleted = player1incompleted + player1incomplete.get(t) + ", ";
+            }
+
+
+            for (int x = 0; x < player2incomplete.size(); x++){
+                player2incompleted = player2incompleted + player2incomplete.get(x) + ", ";
+            }
         }
 
-
-        if(player1completed.equals("")){
+        if(player1completed.isEmpty()){
             player1completed = "None";
         }
+
 
         else{
             player1completed = player1completed.substring(0, player1completed.length() - 2);
         }
-        
 
-        for (int y = 0; y < player2complete.size(); y++){
-            player2completed = player2completed + player2complete.get(y) + ", ";
-        }
-
-        
-
-        if(player2completed.equals("")){
+        if(player2completed.isEmpty()){
             player2completed = "None";
         }
 
@@ -126,11 +158,7 @@ public class LegoSetCompetition{
             player2completed = player2completed.substring(0, player2completed.length() - 2);
         }
 
-        for (int t = 0; t < player1incomplete.size(); t++){
-            player1incompleted = player1incompleted + player1incomplete.get(t) + ", ";
-        }
-
-        if(player1incompleted.equals("")){
+        if(player1incompleted.isEmpty()){
             player1incompleted = "None";
         }
 
@@ -138,19 +166,14 @@ public class LegoSetCompetition{
             player1incompleted = player1incompleted.substring(0, player1incompleted.length() - 2);
         }
 
-        for (int x = 0; x < player2incomplete.size(); x++){
-            player2incompleted = player2incompleted + player2incomplete.get(x) + ", ";
-        }
-
-        if(player2incompleted.equals("")){
+        
+        if(player2incompleted.isEmpty()){
             player2incompleted = "None";
         }
 
         else{
             player2incompleted = player2incompleted.substring(0, player2incompleted.length() - 2);
         }
-
-        
 
         CompetitionLog playerOne = new CompetitionLog(1, player1completed, player1incompleted, player1);
         CompetitionLog playerTwo = new CompetitionLog(2, player2completed, player2incompleted, player2);
