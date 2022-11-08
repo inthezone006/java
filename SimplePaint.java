cca37e4298206d47e3d66814bdf7a153fe5c5f95
Rahul Menon <rahul.menon85280@gmail.com>
2022-11-08 11:54:34 -0500

    everything so far

---
+++ SimplePaint.java
@@ -0,0 +1,54 @@
+import javax.swing.*; 
+import java.awt.*; 
+import java.awt.event.*; 
+
+public class SimplePaint extends JComponent implements Runnable{
+
+    Image image; // the canvas
+    Graphics2D graphics2D;  // this will enable drawing
+    int curX; // current mouse x coordinate
+    int curY; // current mouse y coordinate
+    int oldX; // previous mouse x coordinate
+    int oldY; // previous mouse y coordinate
+
+    JButton enterButton; // button to enter information
+    JTextField strTextField; // text field for input
+
+    SimplePaint paint; // variable of the type SimplePaint
+
+    protected void paintComponent(Graphics g) {
+        if (image == null) {
+            image = createImage(getSize().width, getSize().height);
+
+            // this lets us draw on the image (ie. the canvas)
+            graphics2D = (Graphics2D) image.getGraphics();
+
+            // gives us better rendering quality for the drawing lines 
+            graphics2D.setRenderingHint(RenderingHints.KEY_ANTIALIASING, 
+                                       RenderingHints.VALUE_ANTIALIAS_ON);
+
+            // set canvas to white with default paint color 
+            graphics2D.setPaint(Color.white);
+            graphics2D.fillRect(0, 0, getSize().width, getSize().height);
+            graphics2D.setPaint(Color.black);
+            repaint();
+        }
+        g.drawImage(image, 0, 0, null);
+}
+
+
+    public void run() {
+        JFrame frame = new JFrame("Simple Paint Walkthrough");
+        Container content = frame.getContentPane();
+        content.setLayout(new BorderLayout());
+        paint = new SimplePaint();
+        content.add(paint, BorderLayout.CENTER);
+        frame.setSize(600, 400);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new SimplePaint());
+    }
+}
